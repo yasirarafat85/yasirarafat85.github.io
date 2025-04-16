d4932085d494075c4c1d7ee07cce2d8ad137e980
yasirarafat85 <arafat.bd6@gmail.com>
2025-04-16 14:56:54 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const sheetUrl =
+  "https://spreadsheets.google.com/feeds/list/2PACX-1vSw1hhJh82F5-My06wgU7rkabJOJ_JvnKuJpnrUy7hXguNqBgNlWF3WAL-YbBhNLBQX1Mw4QMMeYdn-/od6/public/values?alt=json";
+
+fetch(sheetUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    const entries = data.feed.entry;
+    const tableBody = document.getElementById("tableBody");
+    const tableHeader = document.getElementById("tableHeader");
+
+    const keys = Object.keys(entries[0])
+      .filter((key) => key.startsWith("gsx$"))
+      .map((key) => key.replace("gsx$", ""));
+
+    keys.forEach((key) => {
+      const th = document.createElement("th");
+      th.textContent = key.toUpperCase();
+      tableHeader.appendChild(th);
+    });
+
+    entries.forEach((row) => {
+      const tr = document.createElement("tr");
+      keys.forEach((key) => {
+        const td = document.createElement("td");
+        td.textContent = row["gsx$" + key]["$t"];
+        tr.appendChild(td);
+      });
+      tableBody.appendChild(tr);
+    });
+
+    $('#sheetTable').DataTable();
+  });
